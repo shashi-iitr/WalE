3072bf9128cec59bb581e2e9162f5dc2bbccdeeb
Shashi Kumar <shashikrishna.sk@gmail.com>
2023-03-23 21:52:27 +0530

    Initial Commit

---
+++ WalE/WalEApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalEApp.swift
+//  WalE
+//
+//  Created by Shashi on 23/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WalEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
